7a6524db5dfd1c454f941941136a8fa8f0a570e8
SWS-5007 <119567356+SWS-5007@users.noreply.github.com>
2023-07-28 22:18:48 -0400

    Add: Added the progress bar

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="progress-bar-container">
+        <div class="progress-bar" :style="{ width: progress + '%' }"></div>
+    </div>
+</template>
+  
+<script>
+export default {
+    props: {
+        progress: {
+            type: Number,
+            required: true,
+        },
+    },
+};
+</script>
+  
+<style>
+.progress-bar-container {
+    width: 80%;
+    margin: auto;
+    margin-top: 20px;
+    background-color: #f2f2f2;
+    border-radius: 4px;
+    overflow: hidden;
+
+}
+
+.progress-bar {
+    height: 20px;
+    background-color: #4caf50;
+    border-radius: 4px;
+    transition: width 0.3s ease;
+}
+</style>
+  
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,14 @@
 <script>
 import { reactive, toRefs } from "vue";
 import GigaVue from "@/components/GigaVue.vue";
+import ProgressBar from '@/components/ProgressBar.vue';
 
 export default {
   name: "Home",
 
   components: {
-    GigaVue
+    GigaVue,
+    ProgressBar
   },
 
   setup() {
@@ -14,7 +16,8 @@ export default {
       petName: "",
       petMood: "Happy",
       userInput: "",
-      showCreatePetForm: true
+      showCreatePetForm: true,
+      progress: 0,
     });
 
     const createPet = () => {
@@ -24,6 +27,11 @@ export default {
 
     const updatePetMood = mood => {
       state.petMood = mood;
+
+      state.progress += 10;
+      if (state.progress > 100) {
+        state.progress = 100;
+      }
     };
 
     return {
@@ -31,28 +39,42 @@ export default {
       createPet,
       updatePetMood
     };
-  }
+  },
 };
 </script>
 
 <template>
   <div class="home">
     <h1>Tamagochi-Vue</h1>
+
     <section>
       <GigaVue :petName="petName" :petMood="petMood" />
     </section>
+
     <h2>{{ petName }}</h2>
+
     <form v-if="showCreatePetForm" @submit.prevent>
       <label for="pet-name">Pet Name</label>
       <input type="text" id="pet-name" v-model="userInput" />
       <button @click="createPet">New Pet</button>
     </form>
-    <div v-else>
-      <button @click="updatePetMood('Happy')">Happy</button>
-      <button @click="updatePetMood('Angry')">Angry</button>
+
+    <div v-else class="button_div">
+      <button @click="updatePetMood('Happy')">Train</button>
+      <button @click="updatePetMood('Angry')">Play</button>
       <button @click="updatePetMood('Sleep')">Sleep</button>
     </div>
+
+    <section>
+      <progress-bar :progress="progress"></progress-bar>
+    </section>
   </div>
 </template>
 
-<style module></style>
+<style>
+.button_div {
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+}
+</style>
